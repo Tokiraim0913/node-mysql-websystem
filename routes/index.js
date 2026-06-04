0c297148914800df9dfd490ab62e07490a8c11fe
片岡季弥 <j2312@matsue-ct.ac.jp>
2026-06-04 07:22:01 +0000

    ToDoタスクを表示させよう

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,13 @@ let todos= [];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ToDo App' });
+  res.render('index', { title: 'ToDo App' , todos: todos,});
 });
 
 router.post('/', function(req, res, next){
   const todo = req.body.add;
   todos.push(todo);
-  res.render('index', {title: 'ToDo App'});
+  res.redirect('/');
 });
 
 module.exports = router;
